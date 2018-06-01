af4b6f881dbc714fd41f7f4ab66e9e478342645c
clyde <clyder@thewiseoptimist.net>
2018-06-01 13:50:25 -0700

    Visualizer Prefs T06.02-Exercise-MakeAPreferenceFragment L6.11 update and tweak todo's

--- Lesson06-Visualizer-Preferences/T06.02-Exercise-MakeAPreferenceFragment/app/src/main/java/android/example/com/visualizerpreferences/SettingsFragment.java
+++ Lesson06-Visualizer-Preferences/T06.02-Exercise-MakeAPreferenceFragment/app/src/main/java/android/example/com/visualizerpreferences/SettingsFragment.java
@@ -5,11 +5,14 @@ import android.support.v7.preference.PreferenceFragmentCompat;
 
 import static android.example.com.visualizerpreferences.R.xml.pref_visualizer;
 
+// TODO completed (2) Create a class called SettingsFragment that extends PreferenceFragmentCompat
 public class SettingsFragment extends PreferenceFragmentCompat {
+
+    // TODO completed (5) In SettingsFragment's onCreatePreferences method add the preference file using the
+    // addPreferencesFromResource method
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         addPreferencesFromResource(R.xml.pref_visualizer);
     }
-
 
 }
