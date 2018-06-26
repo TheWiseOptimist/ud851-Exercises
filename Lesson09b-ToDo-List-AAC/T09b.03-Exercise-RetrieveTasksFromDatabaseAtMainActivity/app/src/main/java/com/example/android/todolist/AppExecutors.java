c1c34e7a39515f66a43a8c2ccc58b2592ca9e0e1
clyde <clyder@thewiseoptimist.net>
2018-06-25 21:14:08 -0700

    TodoList T09b.03-Exercise-RetrieveTasksFromDatabaseAtMainActivity L14.13 add AppExecutors class - not part of assignment, but I wanted a bit more understanding

---
+++ Lesson09b-ToDo-List-AAC/T09b.03-Exercise-RetrieveTasksFromDatabaseAtMainActivity/app/src/main/java/com/example/android/todolist/AppExecutors.java
@@ -0,0 +1,59 @@
+package com.example.android.todolist;
+
+
+import android.content.Context;
+import android.os.Handler;
+import android.os.Looper;
+import android.support.annotation.NonNull;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
+public class AppExecutors {
+
+    private static AppExecutors sInstance;
+    private static final Object LOCK = new Object();
+
+    private Executor diskIO;
+    private Executor networkIO;
+    private Executor mainThread;
+
+    public AppExecutors(Executor diskIO, Executor networkIO, Executor mainThread) {
+        this.diskIO = diskIO;
+        this.networkIO = networkIO;
+        this.mainThread = mainThread;
+    }
+
+    public static AppExecutors getInstance(Context context) {
+        if (sInstance == null) {
+            synchronized (LOCK) {
+                sInstance = new AppExecutors(Executors.newSingleThreadExecutor(),
+                        Executors.newFixedThreadPool(3),
+                        new MainThreadExecutor());
+            }
+        }
+        return sInstance;
+    }
+
+    public Executor getDiskIO() {
+        return diskIO;
+    }
+
+    public Executor getNetworkIO() {
+        return networkIO;
+    }
+
+    public Executor getMainThread() {
+        return mainThread;
+    }
+
+
+    private static class MainThreadExecutor implements Executor {
+        private Handler mainThreadHandler = new Handler(Looper.getMainLooper());
+
+        @Override
+        public void execute(@NonNull Runnable command) {
+            mainThreadHandler.post(command);
+        }
+    }
+}
