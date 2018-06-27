0a417731a8b6de1e7ebb294042f0363bdc4bcf83
clyde <clyder@thewiseoptimist.net>
2018-06-26 22:02:01 -0700

    TodoList T09b.09-Exercise-AddTheViewModel L8.22 complete todo's

--- Lesson09b-ToDo-List-AAC/T09b.09-Exercise-AddTheViewModel/app/src/main/java/com/example/android/todolist/MainViewModel.java
+++ Lesson09b-ToDo-List-AAC/T09b.09-Exercise-AddTheViewModel/app/src/main/java/com/example/android/todolist/MainViewModel.java
@@ -1,11 +1,38 @@
 package com.example.android.todolist;
 
-// TODO (1) make this class extend AndroidViewModel and implement its default constructor
-public class MainViewModel {
+import android.app.Application;
+import android.arch.lifecycle.AndroidViewModel;
+import android.arch.lifecycle.LiveData;
+import android.support.annotation.NonNull;
+import android.util.Log;
 
-    // TODO (2) Add a tasks member variable for a list of TaskEntry objects wrapped in a LiveData
+import com.example.android.todolist.database.AppDatabase;
+import com.example.android.todolist.database.TaskDao;
+import com.example.android.todolist.database.TaskEntry;
 
-    // TODO (4) In the constructor use the loadAllTasks of the taskDao to initialize the tasks variable
+import java.util.List;
 
-    // TODO (3) Create a getter for the tasks variable
+// TODO completed   (1) make this class extend AndroidViewModel and implement its default constructor
+public class MainViewModel extends AndroidViewModel {
+
+    private static final String LOG_TAG = MainViewModel.class.getSimpleName();
+
+    // TODO completed (2) Add a tasks member variable for a list of TaskEntry objects wrapped in a LiveData
+    private LiveData<List<TaskEntry>> tasks;
+
+    public MainViewModel(@NonNull Application application) {
+        super(application);
+        // TODO completed (4) In the constructor use the loadAllTasks of the taskDao to initialize the tasks variable
+        AppDatabase mDb = AppDatabase.getInstance(application); // TODO: 6/26/18
+        Log.d(LOG_TAG, "Actively loading tasks from Database");
+        tasks = mDb.taskDao().loadAllTasks();
+    }
+
+
+    // TODO completed (3) Create a getter for the tasks variable
+    public LiveData<List<TaskEntry>> getTasks() {
+        return tasks;
+    }
+
+
 }
