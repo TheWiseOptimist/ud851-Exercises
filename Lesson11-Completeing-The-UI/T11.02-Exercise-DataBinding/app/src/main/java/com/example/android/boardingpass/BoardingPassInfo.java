8f56826f15ea1fdccaa1b47ff2a1b97a5c651f2a
clyde <clyder@thewiseoptimist.net>
2018-07-08 12:25:29 -0700

    BoardingPass T11.03-Exercise-LandscapeLayout in progress

--- Lesson11-Completeing-The-UI/T11.02-Exercise-DataBinding/app/src/main/java/com/example/android/boardingpass/BoardingPassInfo.java
+++ Lesson11-Completeing-The-UI/T11.02-Exercise-DataBinding/app/src/main/java/com/example/android/boardingpass/BoardingPassInfo.java
@@ -1,20 +1,20 @@
 package com.example.android.boardingpass;
 
 /*
-* Copyright (C) 2016 The Android Open Source Project
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*      http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 import java.sql.Timestamp;
 import java.util.concurrent.TimeUnit;
@@ -44,5 +44,10 @@ public class BoardingPassInfo {
     public long getMinutesUntilBoarding() {
         long millisUntilBoarding = boardingTime.getTime() - System.currentTimeMillis();
         return TimeUnit.MILLISECONDS.toMinutes(millisUntilBoarding);
+    }
+
+    public long[] getHoursAndMinutesUntilBoarding() {
+        return new long[]{TimeUnit.MINUTES.toHours(getMinutesUntilBoarding()),
+                getMinutesUntilBoarding() % 60};
     }
 }
